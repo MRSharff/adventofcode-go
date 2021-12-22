d79c38074f97f43272bbc879d4d8671ca7151516
Mathew Sharff <mrsharff@gmail.com>
2021-12-22 13:35:56 -0800

    Solve day 6 part 2

--- day6/main.go
+++ day6/main.go
@@ -9,12 +9,11 @@ import (
 
 const testInput = `3,4,3,1,2`
 
-func main() {
+var maxDays = 80
 
-	// count the occurrences of each number, then
-	// run the algorithm for the set of numbers that appear, and then
-	// multiply the outcome for each number by how often it occurs
+func main() {
 	solve := func(in string) int {
+
 		agesStrings := strings.Split(in, ",")
 		agesCount := make(map[int]int, 0)
 		for _, a := range agesStrings {
@@ -25,36 +24,63 @@ func main() {
 			agesCount[n]++
 		}
 
-		results := make(map[int]int, len(agesCount))
-		for age := range agesCount {
-			// start with a population of 1 of the initial fish
-			fish := []int{age}
-			for d := 0; d < 80; d++ {
-				for i := range fish {
-					fish[i]--
-				}
-				var newFishOfTheDay []int
-				for i, a := range fish {
-					if a == -1 {
-						newFishOfTheDay = append(newFishOfTheDay, 8)
-						fish[i] = 6
-					}
-				}
-				fish = append(fish, newFishOfTheDay...)
-				//fmt.Printf("After %d day: %v\n", d+1, ages)
-			}
-			results[age] = len(fish)
-		}
-		total := 0
-		for age, fishCount := range results {
-			fmt.Printf("age %d produces %d fish\n", age, fishCount)
-			totalForAge := fishCount * agesCount[age]
-			total += totalForAge
-		}
-		return total
-	}
-	fmt.Println(solve(testInput))
+		// respawningFish holds the fish that have 6 days or less on their spawn timer
+		var respawningFish = make(chan int, 7)
 
+		// newFish hold fish with 7 and 8 days left, such as those fish that are brand new
+		var newFish = make(chan int, 2)
+
+		// set up the queues with the initial state
+		for i := 0; i <= 6; i++ {
+			respawningFish <- agesCount[i]
+		}
+		for i := 7; i <= 8; i++ {
+			newFish <- 0
+		}
+
+		for d := 0; d < maxDays; d++ {
+			// respawning fish create n amount of new fish, and restart their spawn journey
+			n, transitioning := <-respawningFish, <-newFish
+			// add 7 day fish and the restarting fish to the back of the respawningFish queue
+			respawningFish <- n + transitioning
+			newFish <- n
+		}
+		close(respawningFish)
+		close(newFish)
+
+		sum := 0
+		for n := range respawningFish {
+			sum += n
+		}
+		for n := range newFish {
+			sum += n
+		}
+		return sum
+	}
+
+	// tests
+	maxDays = 80
+	got := solve(testInput)
+	expected := 5934
+	if expected != got {
+		fmt.Printf("expected %d, got %d\n", expected, got)
+	} else {
+		fmt.Println("80 day test passed")
+	}
+
+	maxDays = 256
+	got = solve(testInput)
+	expected = 26984457539
+	if expected != got {
+		fmt.Printf("expected %d, got %d\n", expected, got)
+	} else {
+		fmt.Println("256 day test passed")
+	}
+
+	// solve
 	b, _ := ioutil.ReadFile("day6/input.txt")
+	maxDays = 80
+	fmt.Println(solve(string(b)))
+	maxDays = 256
 	fmt.Println(solve(string(b)))
 }
