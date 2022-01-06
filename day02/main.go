35c691e6494d827c459a040c5d6f37698cc731cc
Mathew Sharff <mrsharff@gmail.com>
2022-01-05 22:53:47 -0800

    Refactor day 2

--- day02/main.go
+++ day02/main.go
@@ -15,39 +15,20 @@ var input = func() string {
 // Dive
 // https://adventofcode.com/2021/day/2
 func main() {
-	type command struct {
-		direction string
-		units     int
-	}
-	readCommands := func(input string) []command {
-		lines := strings.Fields(input)
-		commands := make([]command, len(lines)/2)
-		for i := 0; i < len(lines)-1; i += 2 {
-			direction := lines[i]
-			units, err := strconv.Atoi(lines[i+1])
-			if err != nil {
-				panic(err)
-			}
-			commands[i/2] = command{direction, units}
-		}
-		return commands
-	}
+	part1test()
+	answer := part1(input)
+	fmt.Println(answer)
 
-	positionAfterCommands := func(input string) (horizontal int, depth int) {
-		commands := readCommands(input)
-		for _, c := range commands {
-			switch c.direction {
-			case "forward":
-				horizontal += c.units
-			case "down":
-				depth += c.units
-			case "up":
-				depth -= c.units
-			}
-		}
-		return
-	}
+	part2test()
+	fmt.Println(part2(input))
+}
 
+func part1(in string) int {
+	horizontal, depth := positionAfterCommands(in)
+	return horizontal * depth
+}
+
+func part1test() {
 	expectedHorizontal, expectedDepth := 15, 10
 	gotHorizontal, gotDepth := positionAfterCommands("forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2")
 
@@ -60,31 +41,15 @@ func main() {
 		panic("test failed")
 	}
 	fmt.Println("tests passed")
+}
 
-	horizontal, depth := positionAfterCommands(input)
-	answer := horizontal * depth
-	fmt.Println(answer)
+func part2(in string) int {
+	horizontal, depth := positionAfterCommandsIncludingAim(in)
+	return horizontal * depth
+}
 
-	positionAfterCommandsIncludingAim := func(input string) (horizontal int, depth int) {
-		aim := 0
-		commands := readCommands(input)
-		for _, c := range commands {
-			x := c.units
-			switch c.direction {
-			case "forward":
-				horizontal += x
-				depth += aim * x
-			case "down":
-				aim += x
-			case "up":
-				aim -= x
-			}
-		}
-		return
-	}
-
-	expectedHorizontal, expectedDepth = 15, 10
-	gotHorizontal, gotDepth = positionAfterCommandsIncludingAim("forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2")
+func part2test() {
+	gotHorizontal, gotDepth := positionAfterCommandsIncludingAim("forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2")
 
 	expectedAnswer := 15 * 60 // = 900
 	gotAnswer := gotHorizontal * gotDepth
@@ -93,7 +58,56 @@ func main() {
 	} else {
 		fmt.Println("tests passed")
 	}
+}
 
-	h, d := positionAfterCommandsIncludingAim(input)
-	fmt.Println(h * d)
+func positionAfterCommandsIncludingAim(input string) (horizontal int, depth int) {
+	aim := 0
+	commands := readCommands(input)
+	for _, c := range commands {
+		x := c.units
+		switch c.direction {
+		case "forward":
+			horizontal += x
+			depth += aim * x
+		case "down":
+			aim += x
+		case "up":
+			aim -= x
+		}
+	}
+	return
+}
+
+type command struct {
+	direction string
+	units     int
+}
+
+func readCommands(input string) []command {
+	lines := strings.Fields(input)
+	commands := make([]command, len(lines)/2)
+	for i := 0; i < len(lines)-1; i += 2 {
+		direction := lines[i]
+		units, err := strconv.Atoi(lines[i+1])
+		if err != nil {
+			panic(err)
+		}
+		commands[i/2] = command{direction, units}
+	}
+	return commands
+}
+
+func positionAfterCommands(input string) (horizontal int, depth int) {
+	commands := readCommands(input)
+	for _, c := range commands {
+		switch c.direction {
+		case "forward":
+			horizontal += c.units
+		case "down":
+			depth += c.units
+		case "up":
+			depth -= c.units
+		}
+	}
+	return
 }
