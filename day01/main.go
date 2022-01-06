294c9a1c83b1f0e2c0102cf7bc20d611792062a2
Mathew Sharff <mrsharff@gmail.com>
2022-01-05 22:32:58 -0800

    Refactor day 1

--- day01/main.go
+++ day01/main.go
@@ -16,36 +16,10 @@ var adventInput = func() string {
 // Sonar Sweep
 // https://adventofcode.com/2021/day/1
 func main() {
-	reportToDepthList := func(sonarSweepReport string) []int {
-		lines := strings.Fields(sonarSweepReport)
-		depths := make([]int, len(lines))
-		for i, line := range lines {
-			var err error
-			depths[i], err = strconv.Atoi(line)
-			if err != nil {
-				panic(err)
-			}
-		}
-		return depths
-	}
-
-	// part 1 solution
-	countIncreasing := func(sonarSweepReport string) int {
-		lastDepth := math.MaxInt64
-		increaseCount := 0
-		for _, depth := range reportToDepthList(sonarSweepReport) {
-			if depth > lastDepth {
-				increaseCount++
-			}
-			lastDepth = depth
-		}
-		return increaseCount
-	}
-
 	// part 1 tests
 	testInput := "199\n200\n208\n210\n200\n207\n240\n269\n260\n263"
 	expected := 7
-	got := countIncreasing(testInput)
+	got := part1(testInput)
 	if expected != got {
 		fmt.Printf("expected %d, got %d\n", expected, got)
 	} else {
@@ -53,27 +27,12 @@ func main() {
 	}
 
 	// part 1 answers
-	answer := countIncreasing(adventInput)
+	answer := part1(adventInput)
 	fmt.Println(answer)
-
-	// part 2 solution
-	threeMeasurementSlidingWindow := func(sonarSweepReport string) int {
-		depths := reportToDepthList(sonarSweepReport)
-		windowSize := 3
-		increaseCount := 0
-		for i := 0; i < len(depths)-windowSize; i++ {
-			outgoing := depths[i]
-			incoming := depths[i+windowSize]
-			if incoming > outgoing {
-				increaseCount++
-			}
-		}
-		return increaseCount
-	}
 
 	// part 2 tests
 	expected = 5
-	got = threeMeasurementSlidingWindow(testInput)
+	got = part2(testInput)
 	if expected != got {
 		fmt.Printf("expected %d, got %d\n", expected, got)
 	} else {
@@ -81,6 +40,45 @@ func main() {
 	}
 
 	// part 2 result
-	part2Answer := threeMeasurementSlidingWindow(adventInput)
+	part2Answer := part2(adventInput)
 	fmt.Println(part2Answer)
+}
+
+func part1(sonarSweepReport string) int {
+	lastDepth := math.MaxInt64
+	increaseCount := 0
+	for _, depth := range reportToDepthList(sonarSweepReport) {
+		if depth > lastDepth {
+			increaseCount++
+		}
+		lastDepth = depth
+	}
+	return increaseCount
+}
+
+func part2(sonarSweepReport string) int {
+	depths := reportToDepthList(sonarSweepReport)
+	windowSize := 3
+	increaseCount := 0
+	for i := 0; i < len(depths)-windowSize; i++ {
+		outgoing := depths[i]
+		incoming := depths[i+windowSize]
+		if incoming > outgoing {
+			increaseCount++
+		}
+	}
+	return increaseCount
+}
+
+func reportToDepthList(sonarSweepReport string) []int {
+	lines := strings.Fields(sonarSweepReport)
+	depths := make([]int, len(lines))
+	for i, line := range lines {
+		var err error
+		depths[i], err = strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return depths
 }
